93526c44c3290dd27ad363134299c041a8dcf4c2
razaeldotexe <razaelsaputra863@gmail.com>
2026-03-12 15:06:00 +0000

    feat: Add initial Maven project setup and a basic Discord bot application.

---
+++ src/main/java/com/astra/App.java
@@ -0,0 +1,33 @@
+package com.astra;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class App extends ListenerAdapter {
+    public static void main(String[] args) throws Exception {
+        // Ganti "YOUR_BOT_TOKEN" dengan token bot Discord Anda
+        String token = "YOUR_BOT_TOKEN";
+
+        JDABuilder.createDefault(token)
+            .setActivity(Activity.playing("Developing project-astra"))
+            .enableIntents(GatewayIntent.MESSAGE_CONTENT) // Diperlukan untuk membaca isi pesan
+            .addEventListeners(new App())
+            .build();
+        
+        System.out.println("Bot is starting...");
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) return;
+
+        String message = event.getMessage().getContentRaw();
+
+        if (message.equalsIgnoreCase("!ping")) {
+            event.getChannel().sendMessage("Pong!").queue();
+        }
+    }
+}
